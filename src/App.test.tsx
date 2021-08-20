b3b16bff1092e6b03f5477de9dfe999efe725133
Aaron Janes <aaron.janes@gmail.com>
2021-08-19 23:05:56 -0600

    refactor and add AirBnb code style

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import { render, screen , fireEvent} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders button', () => {
-  render(<App />);
-  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
-
-  expect(colorButton).toBeInTheDocument();
-
-
+    render(<App/>);
+    const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+    expect(colorButton).toBeInTheDocument();
 });
 
-
-
-test("Background color of button" , () => {
-  render(<App />);
-  // const backgroundColor = screen.getByRole("button",{backgroundColor:"red"})
-  const backgroundColor = screen.getByRole('button', {name: 'Change to blue'});
-
-  expect(backgroundColor).toHaveStyle({backgroundColor:"red"})
+test("Background color of button", () => {
+    render(<App/>);
+    const backgroundColor = screen.getByRole('button', {name: 'Change to blue'});
+    expect(backgroundColor).toHaveStyle({backgroundColor: "red"})
 })
 
 
-test( "clicking", () =>{
-  render(<App />);
-  const myButton = screen.getByRole("button", {name:"Change to blue"})
-  fireEvent.click(myButton);
-  expect(myButton).toHaveStyle({backgroundColor:"blue"})
+test("clicking", () => {
+    render(<App/>);
+    const myButton = screen.getByRole("button", {name: "Change to blue"})
+    fireEvent.click(myButton);
+    expect(myButton).toHaveStyle({backgroundColor: "blue"})
 
 })
