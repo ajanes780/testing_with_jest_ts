3bdea84c7ec81b01e47c75c6bda1fdce7170d565
Aaron Janes <aaron.janes@gmail.com>
2021-08-19 20:21:05 -0600

    failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,11 @@ import App from './App';
 test('renders button', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+
   expect(colorButton).toBeInTheDocument();
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
